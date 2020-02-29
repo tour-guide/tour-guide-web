510ac18a732cf0c187a93d6c8b9ddff9be3dfed3
Cat Kretzschmar <cat.ktz@gmail.com>
2020-02-28 22:55:17 -0500

    need to merge and see how my bits fit in with everyone elses stuff

--- public/js/stories.js
+++ public/js/stories.js
@@ -1,1 +1,8 @@
-$(".dropdown-trigger").dropdown();
+$(document).ready(function() {
+  const locationEnterOption = $("#locationEnterOption");
+
+  // kick off db call when the save story button is clicked
+  locationEnterOption.on("submit", event => {
+    event.preventDefault();
+  });
+});
